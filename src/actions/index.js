596c38eaf3ab9367b5289e104f06c5fa93589f67
Petch <petchkubb@live.com>
2020-06-29 08:36:48 +0700

    add firebase signup and login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/auth';
-import {EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS} from './types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+} from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -21,6 +26,26 @@ export const loginUser = ({email, password}) => {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then((user) => dispatch({type: LOGIN_USER_SUCCESS, payload: user}));
+      .then((user) => loginUserSucess(dispatch, user))
+      .catch(() => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then((user) => loginUserSucess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
+      });
   };
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({
+    type: LOGIN_USER_FAIL,
+  });
+};
+
+const loginUserSucess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user,
+  });
 };
