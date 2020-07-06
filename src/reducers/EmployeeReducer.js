afafd3b0feed2092e208bc598315642bf460275d
Petch <petchkubb@live.com>
2020-07-06 10:29:51 +0700

    refactor action and add reducer create form

--- src/reducers/EmployeeReducer.js
+++ src/reducers/EmployeeReducer.js
@@ -1,9 +1,15 @@
 import {EMPLOYEE_UPDATE} from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: '',
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      return {...state, [action.payload.props]: action.payload.values};
     default:
       return state;
   }
