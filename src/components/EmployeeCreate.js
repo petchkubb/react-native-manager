f87b1b21b36db0c11355043f164293174887e79e
Petch <petchkubb@live.com>
2020-07-06 10:06:18 +0700

    add create employee ui

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, SafeAreaView, StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
 import {Card, CardSection, Input, Button, Spinner} from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input label="Name" placeholder="Jane" />
+        </CardSection>
+        <CardSection>
+          <Input label="Phone" placeholder="555-5555-55" />
+        </CardSection>
+        <CardSection>
+          <Input label="Name" placeholder="Jane" />
+        </CardSection>
+        <CardSection>
+          <Button title="Create" />
+        </CardSection>
+      </Card>
     );
   }
 }
