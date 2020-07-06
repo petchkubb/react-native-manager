143a69c32c96ade6fcb4a8fd6795957f5630c021
Petch <petchkubb@live.com>
2020-07-06 08:16:44 +0700

    add employee scene

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {connect} from 'react-redux';
+import {Card, CardSection, Input, Button, Spinner} from './common';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+        <Text>EmployeeList</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
