c07ba7b4b1b21014df387ccd5e6d74056b955516
Petch <petchkubb@live.com>
2020-07-06 09:56:04 +0700

    create employee create page

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Scene, Router} from 'react-native-router-flux';
+import {Scene, Router, Actions} from 'react-native-router-flux';
 
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -22,9 +23,14 @@ const RouterComponent = () => {
             component={EmployeeList}
             title="Employees"
             rightTitle="Add"
-            onRight={() => {}}
+            onRight={() => Actions.employeeDetail()}
+            initial
           />
-          {/* <Scene key="employeeDetail" /> */}
+          <Scene
+            key="employeeDetail"
+            component={EmployeeCreate}
+            title="Create Employee"
+          />
         </Scene>
       </Scene>
     </Router>
