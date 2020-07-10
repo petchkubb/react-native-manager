ecd4f806c8e5a2c6c0929d9af4ed19420447dcdd
Petch <petchkubb@live.com>
2020-07-10 22:40:37 +0700

    add list employee

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,34 +1,55 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, FlatList, TouchableWithoutFeedback} from 'react-native';
 import {connect} from 'react-redux';
+import _ from 'lodash';
 import {employeeFetch} from '../actions/EmployeeActions';
-import {Card, CardSection, Input, Button, Spinner} from './common';
+import {CardSection} from './common';
+import {Actions} from 'react-native-router-flux';
 
 class EmployeeList extends Component {
   componentDidMount() {
     this.props.employeeFetch();
   }
+
+  onRowPress = (employee) => () => {
+    Actions.employeeDetail({employee});
+  };
+
+  renderItem = ({item, index}) => {
+    const {name} = item;
+    return (
+      <TouchableWithoutFeedback onPress={this.onRowPress(item)}>
+        <View>
+          <CardSection>
+            <Text>{name}</Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  };
+
   render() {
     return (
       <View>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
+        <FlatList
+          data={this.props.employees}
+          renderItem={this.renderItem}
+          keyExactor={(item) => item.uid}
+        />
       </View>
     );
   }
 }
 
-// const mapStatetoProps = (state) => {
-//   return {
-//     asd: '',
-//   };
-// };
+const mapStatetoProps = (state) => {
+  return {
+    employees: _.map(state.employees, (val, uid) => {
+      return {...val, uid};
+    }),
+  };
+};
 
 const mapDispatchToProps = {
   employeeFetch,
 };
-export default connect(null, mapDispatchToProps)(EmployeeList);
+export default connect(mapStatetoProps, mapDispatchToProps)(EmployeeList);
